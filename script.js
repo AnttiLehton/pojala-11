4f121aebd8c8cdf3fc42fc37d5a4c12bb78b6358
anttilehtonen <anlehton24@students.oamk.fi>
2026-09-17 13:39:37 +0300

    added answers and made button working

---
+++ script.js
@@ -0,0 +1,9 @@
+function naytaVastaukset() {
+    document.getElementById("vastaukset").innerHTML = `
+        <p>1. Palvelu verkkosovellusten julkaisemiseen ja ylläpitoon.</p>
+        <p>2. Koodia voidaan suorittaa ilman oman palvelimen hallintaa.</p>
+        <p>3. Ne tarjoavat pilvipalveluita eri hallinnan tasoilla.</p>
+        <p>4. Se kokoaa samaan kokonaisuuteen liittyvät resurssit yhteen.</p>
+        <p>5. Sovellus julkaistaan Azureen.</p>
+    `;
+}
